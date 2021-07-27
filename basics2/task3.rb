b781a32f765b7a7f69fd9f042c4d8693bfbd047d
David Stof <david.stof@outlook.com>
2021-07-27 23:01:04 +0300

    -

--- basics2/task3.rb
+++ basics2/task3.rb
@@ -7,8 +7,7 @@ my_array = [0, 1]
 loop do
   f = my_array[-2] + my_array[-1]
   break if f > 100
-  my_array.push f
-  index += 1
+  my_array << f
 end
 
 puts my_array.join ', '
--- basics2/task4.rb
+++ basics2/task4.rb
@@ -2,14 +2,10 @@
 Заполнить хеш гласными буквами, где значением будет являтся порядковый номер буквы в алфавите (a - 1).
 =end
 
-alphabet = ("а".."я")
 vowels = %w(а е ё и о у ы э ю я)
 
 result = {}
 
-alphabet.each_with_index do |char, idx|
-  idx += 1
-  result[char] = idx if vowels.include? char
-end
+("а".."я").each_with_index {|char, idx| result[char] = idx+1 if vowels.include? char}
 
 puts result
